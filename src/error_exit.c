4bfbff53fd4e18ec815082693430d9661f1562e5
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-07-09 14:33:15 +0200

    feat: added error exit with a littel sens for pipex

--- src/error_exit.c
+++ src/error_exit.c
@@ -6,16 +6,18 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 13:50:55 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/07/08 12:12:27 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/07/09 14:31:29 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-// Exit the program as failure + frees all
-void	error_exit(t_all *all)
+// prints errno if check is not 0
+void	error_exit_(int check)
 {
-	ft_putendl_fd("Error", 2);
-	ps_free_all(all);
+	if (check != 0)
+	{
+		perror(strerror(errno));
+	}
 	exit(EXIT_FAILURE);
 }
