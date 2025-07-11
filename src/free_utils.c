9d67df92f8450f24fa413165e8f6b1841d1d932a
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-07-11 15:52:06 +0200

    fix: now isert the free fuction

--- src/free_utils.c
+++ src/free_utils.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:18:33 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/07/08 14:06:09 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/07/11 15:46:39 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,22 @@ void	free_all(void)
 	// 	return ;
 	// if (all->ops != NULL)
 	// 	free(all->ops);
+	return ;
+}
+
+// F: free | free split strs and split
+void	free_split(char **split)
+{
+	int	idx;
+
+	if (!split)
+		return ;
+	idx = 0;
+	while (split[idx])
+	{
+		free(split[idx]);
+		idx++;
+	}
+	free(split);
 	return ;
 }
