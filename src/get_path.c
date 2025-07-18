1a941afb3cd52c95f3ec0ff024f1c7d11d00aef0
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-07-18 21:21:06 +0200

    feat: added Path fallback if env is emty

--- src/get_path.c
+++ src/get_path.c
@@ -6,14 +6,14 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 11:32:37 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/07/15 18:28:14 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/07/18 20:13:25 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-// F: Mallocs
-static char	*h_test_path(char *cmd_name, const char *path)
+// F: Mallocs joined_path and split intern
+static char	*h_test_path(const char *cmd_name, const char *path)
 {
 	char	**path_all;
 	int		idx;
@@ -32,7 +32,7 @@ static char	*h_test_path(char *cmd_name, const char *path)
 		joined_path = ft_strjoin(path_all[idx], back_part);
 		if (joined_path == NULL)
 			return (free(back_part), free_split(path_all), NULL);
-		if (access(joined_path, F_OK) == 0)// it can be that i need to check the errnum on error
+		if (access(joined_path, F_OK) == 0)
 			return (free(back_part), free_split(path_all), joined_path);
 		else
 			free(joined_path);
@@ -41,12 +41,18 @@ static char	*h_test_path(char *cmd_name, const char *path)
 	return (free(back_part), free_split(path_all), NULL);
 }
 
-// Mallocs
-char	*get_path_comand(char *cmd_name, const char *envp[])
+// Mallocs joined_path
+char	*get_path_comand(const char *cmd_name, const char *envp[])
 {
-	int		idx;
+	int					idx;
+	static const char	*fallback_path
+		= "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
 
 	idx = 0;
+	if (access(cmd_name, F_OK) == 0)
+		return (ft_strdup(cmd_name));
+	if (envp == NULL || *envp == NULL)
+		return (h_test_path(cmd_name, fallback_path));
 	while (envp[idx])
 	{
 		if (ft_strncmp(envp[idx], "PATH=", 5) == 0)
