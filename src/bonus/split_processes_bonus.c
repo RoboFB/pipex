1d49d904d2128e8cd75dd41fc8ba9f904f30cc08
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-07-22 15:50:51 +0200

    fix(bonus): now the loop for spliting the processes with the correct values of argv

--- src/bonus/split_processes_bonus.c
+++ src/bonus/split_processes_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:01:25 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/07/21 17:49:53 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/07/22 15:34:39 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ pid_t	split_processes_here_doc(
 	curr_pipe = pipes[0];
 	new_pipe = pipes[1];
 	create_pipe(curr_pipe);
-	first_child_here_doc(argv[2], curr_pipe, argv[4], envp);
+	first_child_here_doc(argv[2], curr_pipe, argv[3], envp);
 	count = 4;
-	while (count < argc - 6)
+	while (count < argc - 2)
 	{
 		create_pipe(new_pipe);
 		middle_child(curr_pipe, new_pipe, argv[count], envp);
@@ -39,3 +39,9 @@ pid_t	split_processes_here_doc(
 	close_one_pip(curr_pipe);
 	return (last_child_pid);
 }
+
+// ./pipex here_doc LIMITER cmd cmd1 outfile    6
+//    0       1        2     3   4      5
+
+// ./pipex here_doc LIMITER cmd cmd1 cmd2 outfile     7
+//    0       1        2     3   4    5      6
